72c893dc2a17b7aafc56073d2f974b218956a3bd
Ydemae <113693952+Ydemae@users.noreply.github.com>
2024-12-08 10:39:11 +0100

    correct DisplayTensor

--- AiTraining/convertModel/DisplayTensors.py
+++ AiTraining/convertModel/DisplayTensors.py
@@ -1,26 +1,16 @@
 import tensorflow as tf
-import numpy as np
 
-# Load the TFLite model
-interpreter = tf.lite.Interpreter(model_path="model.tflite")
+tflite_model_path = "model.tflite"
+
+interpreter = tf.lite.Interpreter(model_path=tflite_model_path)
 interpreter.allocate_tensors()
-
-# Check input and output details
 input_details = interpreter.get_input_details()
 output_details = interpreter.get_output_details()
 
-print("Input details:", input_details)
-print("Output details:", output_details)
+print("Input Tensors:")
+for input_detail in input_details:
+    print(f"Name : {input_detail['name']}\nTensor shape : {input_detail['shape']}")
 
-# Prepare a dummy input (depending on your model's input size and shape)
-input_data = np.random.random_sample(input_details[0]['shape']).astype(np.float32)
-
-# Set the input tensor
-interpreter.set_tensor(input_details[0]['index'], input_data)
-
-# Run the model (invoke the interpreter)
-interpreter.invoke()
-
-# Get the output
-output_data = interpreter.get_tensor(output_details[0]['index'])
-print("Output:", output_data)
+print("\nOutput Tensors:")
+for output_detail in output_details:
+    print(f"Name : {output_detail['name']}\nTensor shape : {output_detail['shape']}")

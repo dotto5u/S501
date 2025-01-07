6b2e0902ab5e2bf70c01b01d1cbcf7358b5542de
Ydemae <113693952+Ydemae@users.noreply.github.com>
2025-01-07 10:15:10 +0100

    test models and model conversion

--- AiTraining/YoloTraining/convertToTflite.py
+++ AiTraining/YoloTraining/convertToTflite.py
@@ -1,6 +1,6 @@
 from ultralytics import YOLO
 
-model = YOLO("best.pt")
+model = YOLO("train7Last.pt")
 
 res = model.export(format="tflite")
 
---
+++ AiTraining/YoloTraining/runInferenceOnValid.py
@@ -0,0 +1,31 @@
+import cv2
+import torch
+from ultralytics import YOLO
+import matplotlib.pyplot as plt
+import os
+
+modelPath = "train7Best.pt"
+
+model = YOLO(modelPath)
+
+image_dir_path = './dataset/valid/images'
+output_dir_path = './testModelOutputBesst'
+
+
+allFiles = os.listdir(image_dir_path)
+for file in allFiles:
+    if not str(file).endswith('.jpg'):
+        continue
+
+    filePath = os.path.join(image_dir_path, file)
+    image = cv2.imread(filePath, cv2.COLOR_RGB2BGR)
+    results = model(image)
+
+    annotated_frame = results[0].plot()
+
+    #plt.figure(figsize=(10, 10))
+    #plt.imshow(cv2.cvtColor(annotated_frame, cv2.COLOR_RGB2BGR))
+    #plt.axis('off')
+    #plt.show()
+
+    cv2.imwrite(os.path.join(output_dir_path, file), annotated_frame)
